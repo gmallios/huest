abffc5e36ccf453e65fd4a75cda9108d9b89d2c2
Gregory Mallios <gregmallios@gmail.com>
2022-06-27 04:14:27 +0300

    Add debug api link

--- src/hue_api/mod.rs
+++ src/hue_api/mod.rs
@@ -3,10 +3,12 @@ use rocket::Route;
 pub mod hue_device_model;
 pub mod hue_config_controller;
 pub mod hue_config_model;
+pub mod hue_mdns;
 
 pub use hue_config_controller as config_controller;
 pub use hue_config_model as config_model;
 pub use hue_device_model as device_model;
+pub use hue_mdns as mdns;
 
 
 mod hue_routes;
@@ -24,12 +26,24 @@ pub fn hue_routes() -> Vec<Route> {
         route_config_no_user,
         route_config_with_uid,
         route_config_post,
-        press_link_button
+        press_link_button,
+        is_link_button_pressed
     ]
 }
 
 // Debug routes
 #[get("/slink")]
-fn press_link_button(api_state: &rocket::State<hue_config_controller::HueConfigControllerState>) {
+fn press_link_button(api_state: &rocket::State<hue_config_controller::HueConfigControllerState>) -> String {
     api_state.get_controller().press_link_button();
+    "Link button pressed".to_string()
+}
+
+#[get("/islink")]
+fn is_link_button_pressed(api_state: &rocket::State<hue_config_controller::HueConfigControllerState>) -> String {
+    println!("is_link_button_pressed {} ", api_state.get_controller().is_link_button_pressed());
+    if api_state.get_controller().is_link_button_pressed() {
+        "Link button pressed".to_string()
+    } else {
+        "Link button not pressed".to_string()
+    }
 }
