4cf899ce8284baaa8c10c826b5bcae9b9f5f827d
Gregory Mallios <gregmallios@gmail.com>
2022-07-18 15:23:23 +0300

    Works

--- src/bridge.rs
+++ src/bridge.rs
@@ -1,94 +1,73 @@
+use default_net::{Interface, gateway};
 // Use clap to parse command line arguments
 //use default_net::{self, gateway, Interface};
-use log::warn;
+use log::{warn, debug};
 use once_cell::sync::Lazy;
 use std::sync::{Arc, RwLock};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct BridgeParams {
-   // pub https: bool,
+    pub https: bool,
     pub https_port: u16,
     pub http_port: u16,
-   // pub mac_address: String,
-   // pub bind_ip: String,
-   // pub local_ip: String,
-    //pub gateway: String,
-    //pub iface: Interface,
+    pub mac_address: String,
+    pub bind_ip: String,
+    pub local_ip: String,
+    pub gateway_ip: String,
+    pub iface: Interface,
 }
 
 // Source of truth?
 // Should be used exclusively for reads after initialization.
-// lazy_static! {
-//     pub static ref BRIDGE_PARAMS: Arc<RwLock<BridgeParams>> = Arc::new(RwLock::new(BridgeParams {
-//         initialized: false,
-//         https: true,
-//         https_port: 443,
-//         http_port: 80,
-//         mac_address: String::from("A"),
-//         bind_ip: "0.0.0.0".to_string(),
-//         local_ip: String::new(),
-//         gateway: String::new(),
-//         iface: default_net::get_default_interface().unwrap(),
-//     }));
-// }
 
 pub static BRIDGE_PARAMS: Lazy<Arc<RwLock<BridgeParams>>> = Lazy::new(|| {
+
+    let iface = get_iface();
+    let local_ip = iface.ipv4.first().unwrap().clone().addr.to_string();
+    let mut mac_address: String = String::new();
+    let mut gateway_ip: String = String::new();
+    if let (Some(mac), Some(gate)) = (&iface.mac_addr, &iface.gateway ) {
+        mac_address = mac.to_string();
+        gateway_ip = gate.ip_addr.to_string();
+    } else {
+        warn!("No MAC address or gateway IP found for interface {}", iface.name);
+        std::process::exit(1);
+    }
+
     let params = BridgeParams {
-       // https: true,
+        https: true,
         https_port: 443,
         http_port: 80,
-        // mac_address: String::from("A"),
-        // bind_ip: "0.0.0.0".to_string(),
-        // local_ip: String::new(),
-        // gateway: String::new(),
-        // iface: default_net::get_default_interface().unwrap(),
+        bind_ip: String::from("0.0.0.0"),
+        mac_address,
+        local_ip,
+        gateway_ip,
+        iface,
     };
+
+    debug!("params: {:?}", params);
 
     Arc::new(RwLock::new(params))
 });
 
-fn init() {
-    let mut bridge_params = BRIDGE_PARAMS.write().unwrap();
 
-   // bridge_params.iface = get_iface();
-    // bridge_params.local_ip = bridge_params.iface.ipv4.first().unwrap().clone().addr.to_string();
-
-    // if let Some(mac) = &bridge_params.iface.mac_addr {
-    //     bridge_params.mac_address = mac.to_string();
-    // } else {
-    //     warn!("No MAC address found for interface {}", bridge_params.iface.name);
-    //     std::process::exit(1);
-    // }
-
-    // if let Some(gateway) = &bridge_params.iface.gateway {
-    //     bridge_params.gateway = gateway.ip_addr.to_string();
-    // } else {
-    //     warn!("No gateway found for interface {}", bridge_params.iface.name);
-    //     std::process::exit(1);
-    // }
-
-   // bridge_params.initialized = true;
+fn get_iface() -> Interface {
+    // TODO: Check parameters for ovveride else use default_net::get_default_interface()
+    let iface = match default_net::get_default_interface() {
+        Ok(iface) => iface,
+        Err(_) => {
+            warn!("Failed to get default interface!");
+            std::process::exit(1);
+        }
+    };
+    iface
 }
-
-// fn get_iface() -> Interface {
-//     // TODO: Check parameters for ovveride else use default_net::get_default_interface()
-//     let iface = match default_net::get_default_interface() {
-//         Ok(iface) => iface,
-//         Err(_) => {
-//             warn!("Failed to get default interface!");
-//             std::process::exit(1);
-//         }
-//     };
-//     iface
-// }
 
 /* Utility functions */
 pub fn get_mac_addr() -> String {
-    String::from("B")
-   // return BRIDGE_PARAMS.read().unwrap().mac_address.to_string();
+    return BRIDGE_PARAMS.read().unwrap().mac_address.to_string();
 }
 
 pub fn get_local_ip() -> String {
-    String::from("A")
-   // return BRIDGE_PARAMS.read().unwrap().local_ip.to_string();
+    return BRIDGE_PARAMS.read().unwrap().local_ip.to_string();
 }
